2ed2f725ec4871d7c263fa29ee1c255136555b6f
Manuel Gomez <jmgc@virket.com>
2024-04-30 18:43:45 -0600

    Caso de uso - Now Playing

---
+++ src/core/entities/movie.entity.ts
@@ -0,0 +1,11 @@
+
+// Para usar mi propio Modelo - Resource/tranform as Laravel
+export interface Movie {
+    id: number;
+    title: string;
+    description: string;
+    releaseDate: Date;
+    rating: number;
+    poster: string;
+    backdrop: string;
+}
---
+++ src/core/use-cases/movies/now-playing.use-case.ts
@@ -0,0 +1,17 @@
+import { HttpAdapter } from "../../../adapters/http/http.adapter";
+import { NowPlayingResponse } from "../../../infrastructure/interfaces/movie-db.responses";
+import type { Movie } from "../../entities/movie.entity";
+
+export const moviesNowPlayingUseCase = async ( fetcher: HttpAdapter): Promise<Movie[]> => {
+    // casos de uso sean agnosticos - que no dependan dependencias de paquetes de terceros
+    try {
+        const nowPlaying = await fetcher.get<NowPlayingResponse>('/now_playing');
+        console.log(nowPlaying);
+        // transform data - mapper
+        return [];
+        
+    } catch (error) {
+        console.log(error);
+        throw new Error("Error fetching movies - nowPlaying");
+    }
+}
