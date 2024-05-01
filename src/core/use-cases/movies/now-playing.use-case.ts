c3421a4afbce6b52d37a6046e984a9c09956550d
Manuel Gomez <jmgc@virket.com>
2024-05-01 08:38:37 -0600

    CustomHook - useMovies

--- src/core/use-cases/movies/now-playing.use-case.ts
+++ src/core/use-cases/movies/now-playing.use-case.ts
@@ -2,7 +2,7 @@ import { HttpAdapter } from "../../../adapters/http/http.adapter";
 import { NowPlayingResponse } from "../../../infrastructure/interfaces/movie-db.responses";
 import type { Movie } from "../../entities/movie.entity";
 
-export const moviesNowPlayingUseCase = async ( fetcher: HttpAdapter): Promise<Movie[]> => {
+export const moviesNowPlayingUseCase = async ( fetcher: HttpAdapter ): Promise<Movie[]> => {
     // casos de uso sean agnosticos - que no dependan dependencias de paquetes de terceros
     try {
         const nowPlaying = await fetcher.get<NowPlayingResponse>('/now_playing');
---
+++ src/presentation/hooks/useMovies.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react'
+import { Movie } from '../../core/entities/movie.entity';
+import * as UseCases from '../../core/use-cases';
+import { movieDBFetcher } from '../../adapters/movieDb.adapter';
+
+export const useMovies = () => {
+  
+    const [isLoading, setIsLoading] = useState(true);
+    const [nowPlaying, setnowPlaying] = useState<Movie[]>([]);
+
+    useEffect(() => {
+        initialLoad();
+    }, []);
+
+    const initialLoad = async() => {
+        // Solicita indicar un adaptador fetcher
+        const nowPlayingMovies = await UseCases.moviesNowPlayingUseCase(movieDBFetcher);
+    }
+
+    return {
+
+    }
+}
